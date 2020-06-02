34bf582705d91abbc0a042dc38662f537afdd1d4
mathislrn <mathis.lrn061198@gmail.com>
2020-06-02 11:16:58 +0200

    tp2: Project finished

--- tp-2-ant-aco/main.py
+++ tp-2-ant-aco/main.py
@@ -1,105 +1,156 @@
-# import random as rand
-# import networkx as nx
-# import matplotlib.pyplot as plt
-
-# NB_NODE_MAX=100
-# CENTRALISATION_MAX=(NB_NODE_MAX/2)
-# NB_LONG_MAX=100
-
-# def init():
-#     G=nx.Graph()
-#     for x in range(NB_NODE_MAX):
-#         G.add_edge(rand.randrange(0, CENTRALISATION_MAX), rand.randrange(0, CENTRALISATION_MAX), lenght=rand.randrange(0, NB_LONG_MAX))
-#     print(G.edges.data())
-#     shortest_path = nx.shortest_path(G)
-#     nx.draw(G, with_labels=True, font_weight='bold')
-#     plt.show()
-
-# if __name__ == "__main__":
-#     init()
-
-import random as rn
+import random as rand
+import networkx as nx
+import matplotlib.pyplot as plt
+from numpy.random import choice
+import time
 import numpy as np
-from numpy.random import choice as np_choice
 
-class AntColony(object):
+NODE_MAX_QTY = 50
+CENTRALISATION_MAX = (NODE_MAX_QTY / 2)
+MAX_LENGHT_AMOUNT = 100
 
-    def __init__(self, distances, n_ants, n_best, n_iterations, decay, alpha=1, beta=1):
-        """
-        Args:
-            distances (2D numpy.array): Square matrix of distances. Diagonal is assumed to be np.inf.
-            n_ants (int): Number of ants running per iteration
-            n_best (int): Number of best ants who deposit pheromone
-            n_iteration (int): Number of iterations
-            decay (float): Rate it which pheromone decays. The pheromone value is multiplied by decay, so 0.95 will lead to decay, 0.5 to much faster decay.
-            alpha (int or float): exponenet on pheromone, higher alpha gives pheromone more weight. Default=1
-            beta (int or float): exponent on distance, higher beta give distance more weight. Default=1
-        Example:
-            ant_colony = AntColony(german_distances, 100, 20, 2000, 0.95, alpha=1, beta=2)          
-        """
-        self.distances  = distances
-        self.pheromone = np.ones(self.distances.shape) / len(distances)
-        self.all_inds = range(len(distances))
-        self.n_ants = n_ants
-        self.n_best = n_best
-        self.n_iterations = n_iterations
-        self.decay = decay
-        self.alpha = alpha
-        self.beta = beta
+TRY_AMOUNT = 100
+ANT_QTY = 100
 
-    def run(self):
-        shortest_path = None
-        all_time_shortest_path = ("placeholder", np.inf)
-        for i in range(self.n_iterations):
-            all_paths = self.gen_all_paths()
-            self.spread_pheronome(all_paths, self.n_best, shortest_path=shortest_path)
-            shortest_path = min(all_paths, key=lambda x: x[1])
-            print (shortest_path)
-            if shortest_path[1] < all_time_shortest_path[1]:
-                all_time_shortest_path = shortest_path            
-            self.pheromone * self.decay            
-        return all_time_shortest_path
+# Start where all ants start from...
+START_NODE = 0
+# Finish node where the food is...
+FINISH_NODE = 19
 
-    def spread_pheronome(self, all_paths, n_best, shortest_path):
-        sorted_paths = sorted(all_paths, key=lambda x: x[1])
-        for path, dist in sorted_paths[:n_best]:
-            for move in path:
-                self.pheromone[move] += 1.0 / self.distances[move]
-
-    def gen_path_dist(self, path):
-        total_dist = 0
-        for ele in path:
-            total_dist += self.distances[ele]
-        return total_dist
-
-    def gen_all_paths(self):
-        all_paths = []
-        for i in range(self.n_ants):
-            path = self.gen_path(0)
-            all_paths.append((path, self.gen_path_dist(path)))
-        return all_paths
-
-    def gen_path(self, start):
-        path = []
-        visited = set()
-        visited.add(start)
-        prev = start
-        for i in range(len(self.distances) - 1):
-            move = self.pick_move(self.pheromone[prev], self.distances[prev], visited)
-            path.append((prev, move))
-            prev = move
-            visited.add(move)
-        path.append((prev, start)) # going back to where we started    
-        return path
-
-    def pick_move(self, pheromone, dist, visited):
-        pheromone = np.copy(pheromone)
-        pheromone[list(visited)] = 0
-
-        row = pheromone ** self.alpha * (( 1.0 / dist) ** self.beta)
-
-        norm_row = row / row.sum()
-        move = np_choice(self.all_inds, 1, p=norm_row)[0]
-        return move
+# Initialize Networkx Graph
+G = nx.Graph()
 
 
+def init():
+    # Generate random edges based on random start and end node with random 'length'
+    for x in range(NODE_MAX_QTY):
+        G.add_edge(rand.randrange(0, CENTRALISATION_MAX), rand.randrange(
+            0, CENTRALISATION_MAX), length=rand.randrange(0, MAX_LENGHT_AMOUNT), pheromone=1)
+
+
+def launch():
+    # Start timer
+    start_time = time.time()
+    for i in range(TRY_AMOUNT):
+        for j in range(ANT_QTY):
+            node = START_NODE
+            visited_nodes = [START_NODE]
+            while node != FINISH_NODE:
+                node = choose_next_node(node, visited_nodes)
+                if node == -1:
+                    break
+                visited_nodes.append(node)
+            if node == FINISH_NODE:
+                spread_pheromone(visited_nodes)
+        evaporate_pheromone()
+    get_shortest_path()
+    # End timer & print resolve time
+    print(f'Path found in {round(time.time() - start_time , 3)} seconds')
+    plt.show()
+
+
+def choose_next_node(node, visited_nodes):
+    # G.degree gives the number of edges adjacent to the node
+    some_length = G.degree(node, weight='length')
+    # print(some_length)
+    somme_pheromones = G.degree(node, weight='pheromone')
+    # print(somme_pheromones)
+    probs = []
+    arrives = []
+
+    if some_length == 0 or somme_pheromones == 0:
+        return -1
+
+    for node1, node2, data in G.edges(node, data=True):
+        if visited_nodes.count(node2) <= 0:
+            proba = (((1 - (data['length'] / some_length)) *
+                      0.05) + (data['pheromone'] / somme_pheromones * 0.95))
+            probs.append(proba)
+            arrives.append(node2)
+
+    if len(arrives) == 0:
+        return -1
+
+    probs = np.array(probs)
+    probs /= probs.sum()
+    res = choice(arrives, size=1, p=probs)[0]
+
+    return res
+
+
+def spread_pheromone(visited_nodes):
+    graph_path_length = G.size(weight='length')
+    distance = 0
+    node_iterations = iter(visited_nodes)
+    previous_node = next(node_iterations)
+
+    for n in node_iterations:
+        distance += G[previous_node][n]['length']
+        previous_node = n
+
+    pheromone_qty = graph_path_length / distance
+    node_iterations = iter(visited_nodes)
+    previous_node = next(node_iterations)
+
+    for n in node_iterations:
+        G[previous_node][n]['pheromone'] += round(pheromone_qty)
+        previous_node = n
+
+
+def evaporate_pheromone():
+    for node1, node2 in G.edges:
+        G[node1][node2]['pheromone'] = round(G[node1]
+                                             [node2]['pheromone'] * 0.05)
+
+
+def get_shortest_path():
+    path = [FINISH_NODE]
+    node = FINISH_NODE
+    attempts = 0
+    while node != START_NODE:
+        maximum = 0
+        attempts += 1
+
+        if attempts > 1000:
+            print('Missed')
+            print(path)
+            return None
+
+        for node1, node2, data in G.edges(node, data=True):
+            if data['pheromone'] > maximum and node2 not in path:
+                maximum = data['pheromone']
+                node = node2
+
+        path.append(node)
+
+    # Filter if node is or is not in found path
+    nodes_not_in_path = [(u, v) for (u, v, d) in G.edges(data=True) if u not in path or v not in path]
+    nodes_in_path = [(u, v) for (u, v, d) in G.edges(data=True) if u in path and v in path]
+
+    pos = nx.spring_layout(G)  # positions for all nodes
+
+    # nodes
+    nx.draw_networkx_nodes(G, pos, node_size=250)
+
+    # edges
+    nx.draw_networkx_edges(G, pos, edgelist=nodes_not_in_path,
+                           width=2)
+    nx.draw_networkx_edges(G, pos, edgelist=nodes_in_path,
+                           width=3, edge_color=(200.0 / 255.0, 0.0 / 255.0, 0.0 / 255.0))
+
+    # labels
+    nx.draw_networkx_labels(G, pos, font_size=10, font_family='sans-serif')
+
+    print(f'Shortest path: {path}')
+
+
+def main():
+    print(f"Start Node : {START_NODE} | End Node : {FINISH_NODE}")
+    init()
+    print("🟢 Started")
+    launch()
+    print("🔴 Ended")
+
+
+if __name__ == '__main__':
+    main()
